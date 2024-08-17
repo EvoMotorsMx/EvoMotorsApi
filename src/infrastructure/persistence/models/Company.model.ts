6b431ee3f484193a2780c71290455596b08e1bee
Amílcar Sánchez <45496427+amilcaralex97@users.noreply.github.com>
2024-08-17 12:15:51 -0600

    Sc 85 company diseno modelo (#14)
    * feat(Company): company model design
    
    * feat(Company): missing files implemented

---
+++ src/infrastructure/persistence/models/Company.model.ts
@@ -0,0 +1,60 @@
+import mongoose, { Schema, Document } from "mongoose";
+import { v4 as uuidV4 } from "uuid";
+
+export interface CompanyDocument extends Document {
+  name: string;
+  lastName: string;
+  city: string;
+  state: string;
+  country: string;
+  phone: string;
+  email: string;
+  users: string[];
+}
+
+const companySchema = new Schema<CompanyDocument>(
+  {
+    _id: {
+      type: String,
+      default: () => uuidV4(),
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    users: [
+      {
+        type: String,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  },
+);
+
+companySchema.set("autoIndex", false);
+
+const Company = mongoose.model<CompanyDocument>("Company", companySchema);
+
+export default Company;
