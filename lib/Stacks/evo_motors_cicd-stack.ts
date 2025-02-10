dc38e83b46887aafb713563c6f584e9cf396e324
Amilcar Sanchez <amilcaralex97@gmail.com>
2025-02-10 10:25:50 -0600

    Update CI/CD stack to install the latest AWS CDK version before synthesis

--- lib/Stacks/evo_motors_cicd-stack.ts
+++ lib/Stacks/evo_motors_cicd-stack.ts
@@ -19,7 +19,11 @@ export class EvoMotorsCiCdStack extends cdk.Stack {
       pipelineName: "EvoMotorsPipeline",
       synth: new ShellStep("Synth", {
         input: CodePipelineSource.gitHub("EvoMotorsMx/EvoMotorsApi", "master"),
-        commands: ["npm ci", "npx cdk synth"],
+        commands: [
+          "npm install -g aws-cdk@latest", // 🔹 Instala la última versión de CDK
+          "npm ci",
+          "npx cdk synth",
+        ],
         primaryOutputDirectory: "cdk.out",
       }),
     });
