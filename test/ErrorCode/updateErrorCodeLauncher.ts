2d5669b4ec91d7e8c9bd291f1005bb982ae40c72
Amilcar Sanchez <amilcaralex97@gmail.com>
2025-02-19 10:57:36 -0600

    Refactor ErrorCode launchers to use environment variables for headers

--- test/ErrorCode/updateErrorCodeLauncher.ts
+++ test/ErrorCode/updateErrorCodeLauncher.ts
@@ -6,8 +6,8 @@ handler(
       errorCodeId: "",
     },
     headers: {
-      idtoken: "",
-      Authorization: "",
+      idtoken: process.env.ID_TOKEN,
+      Authorization: process.env.AUTHORIZATION,
     },
     requestContext: {
       http: {
