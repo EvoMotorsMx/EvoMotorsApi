9b67791927a5ba0d0cd05b8925d4d23aedfec6dd
Amilcar Sanchez <amilcaralex97@gmail.com>
2025-02-20 16:06:01 -0600

    Refactor Car entity to remove unused properties and simplify imports

--- src/core/domain/entities/Car.ts
+++ src/core/domain/entities/Car.ts
@@ -1,27 +1,13 @@
-import {
-  CarModel,
-  Certificate,
-  Customer,
-  ErrorCode,
-  Remission,
-  Witness,
-} from "./index";
+import { CarModel, Certificate, Customer, Remission } from "./index";
 
 export class Car {
   constructor(
-    public mileage: number,
-    public tankStatus: number,
-    public damageImageUrl: string[],
-    public damageStatusDescription: string,
-    public scannerDescription: string,
     public vin: string,
     public plates: string,
     public carModelId: CarModel,
     public customerId: Customer,
     public certificateId?: Certificate,
     public remissions?: Remission[],
-    public witnesses?: Witness[],
-    public errorCodes?: ErrorCode[],
     public files?: File[],
     public _id?: string,
   ) {}
