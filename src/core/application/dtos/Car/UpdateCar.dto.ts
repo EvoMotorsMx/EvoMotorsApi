a98edb615b32690988c05cbc18e9a0a858ac8553
Amilcar Sanchez <amilcaralex97@gmail.com>
2025-02-20 16:05:24 -0600

    Refactor UpdateCarDTO to simplify properties and enhance clarity

--- src/core/application/dtos/Car/UpdateCar.dto.ts
+++ src/core/application/dtos/Car/UpdateCar.dto.ts
@@ -1,25 +1,10 @@
-import { CombustionType, EngineType } from "../../../../shared/enums";
-
 export type UpdateCarDTO = {
-  name?: string;
-  brandId?: string;
-  carModelId?: string;
-  year?: string;
-  engineSize?: string;
-  cylinder?: number;
-  combustion?: CombustionType;
-  engineType?: EngineType;
-  mileage?: number;
-  tankStatus?: number;
-  damageImageUrl?: string[];
-  damageStatusDescription?: string;
-  scannerDescription?: string;
   vin?: string;
   plates?: string;
-  customerId?: string;
+  carModelId?: string;
   certificateId?: string;
   remissions?: string[];
-  witnesses?: string[];
+  customerId?: string;
   errorCodes?: string[];
   files?: string[];
 };
