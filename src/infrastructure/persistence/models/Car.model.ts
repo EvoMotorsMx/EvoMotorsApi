19a28a7a35aa8f53d13cbbc465e42c75760a495b
Amilcar Sanchez <amilcaralex97@gmail.com>
2025-02-20 16:07:43 -0600

    Refactor Car model to remove unused fields and simplify schema

--- src/infrastructure/persistence/models/Car.model.ts
+++ src/infrastructure/persistence/models/Car.model.ts
@@ -7,22 +7,14 @@ import {
   ErrorCode,
   File,
   Remission,
-  Witness,
 } from "../../../core/domain/entities";
 
 export interface CarDocument extends Document {
-  mileage: number;
-  tankStatus: number;
-  damageImageUrl: string[];
-  damageStatusDescription: string;
-  scannerDescriptionUrl: string[];
-  scannerDescription: string;
   vin: string;
   plates: string;
   carModelId: CarModel;
   certificateId: Certificate;
   remissions: Remission[];
-  witnesses: Witness[];
   customerId: Customer;
   errorCodes: ErrorCode[];
   files: File[];
@@ -39,39 +31,7 @@ const carSchema = new Schema<CarDocument>(
       required: true,
       ref: "CarModel",
     },
-    witnesses: [
-      {
-        type: String,
-        ref: "Witness",
-      },
-    ],
-    mileage: {
-      type: Number,
-      required: true,
-    },
-    tankStatus: {
-      type: Number,
-      required: true,
-    },
-    damageImageUrl: [
-      {
-        type: String,
-      },
-    ],
-    damageStatusDescription: {
-      type: String,
-      required: true,
-    },
-    scannerDescriptionUrl: [
-      {
-        type: String,
-        required: true,
-      },
-    ],
-    scannerDescription: {
-      type: String,
-      required: true,
-    },
+
     vin: {
       type: String,
       required: true,
