ae57f34d4109bad4ca54c8403a8ee569fff702aa
Amilcar Sanchez <amilcaralex97@gmail.com>
2025-02-20 16:05:16 -0600

    Refactor CreateCarDTO to streamline properties and remove unused fields

--- src/core/application/dtos/Car/CreateCar.dto.ts
+++ src/core/application/dtos/Car/CreateCar.dto.ts
@@ -1,25 +1,10 @@
-import { CombustionType, EngineType } from "../../../../shared/enums";
-
 export type CreateCarDTO = {
-  name: string;
-  brandId: string;
-  carModelId: string;
-  year: string;
-  engineSize: string;
-  cylinder: number;
-  combustion: CombustionType;
-  engineType: EngineType;
-  customerId: string;
-  mileage: number;
-  tankStatus: number;
-  damageImageUrl?: string[];
-  damageStatusDescription: string;
-  scannerDescription: string;
   vin: string;
   plates: string;
-  certificateId?: string;
-  remissions?: string[];
-  witnesses?: string[];
-  errorCodes?: string[];
-  files?: string[];
+  carModelId: string;
+  certificateId: string;
+  remissions: string[];
+  customerId: string;
+  errorCodes: string[];
+  files: string[];
 };
