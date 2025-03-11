ab5f2c73aec9d06b41f37f4e0f9a596060d35655
Amilcar Sanchez <amilcaralex97@gmail.com>
2025-03-10 18:58:17 -0600

    Add Receipt entity to manage installation details and associated data

---
+++ src/core/domain/entities/Receipt.ts
@@ -0,0 +1,26 @@
+import { Car, ErrorCode, ProductCompatibility, User, Witness } from "./";
+
+export class Receipt {
+  constructor(
+    public signImage: string,
+    public installationStatus: "pending" | "completed",
+    public tankStatus: number,
+    public mileage: number,
+    public damageImages: string[],
+    public scannerDescriptionImages: string[],
+    public cognitoId: User,
+    public carId?: Car,
+    public installationEndDate?: Date,
+    public damageStatusDescription?: string,
+    public scannerDescription?: string,
+    public witnesses?: Witness[],
+    public productInstalled?: ProductCompatibility[],
+    public _id?: string,
+  ) {}
+
+  setId(id: string) {
+    if (!this._id) {
+      this._id = id;
+    }
+  }
+}
