619b4996173b0982841c51d6f81f09f2d1421737
Amilcar Sanchez <amilcaralex97@gmail.com>
2025-03-12 10:28:00 -0600

    Add receipt handling tests and update request structure for receipt operations

--- test/Receipt/createReceiptLauncher.ts
+++ test/Receipt/createReceiptLauncher.ts
@@ -1,5 +1,7 @@
 import { handler } from "../../src/infrastructure/web/routes/Receipt/handler";
 
+const binaryImage = Buffer.from("fakeImageData", "binary").toString("binary");
+
 handler(
   {
     requestContext: {
@@ -8,20 +10,24 @@ handler(
       },
     },
     headers: {
-      headers: {
-        idtoken: process.env.ID_TOKEN,
-        Authorization: process.env.AUTHORIZATION,
-      },
+      idtoken: process.env.ID_TOKEN,
+      Authorization: process.env.AUTHORIZATION,
     },
     body: JSON.stringify({
-      name: "A1",
-      brandId: "dc10bea8-0ae9-471e-91d5-18df8c16448f",
-      year: ["2019", "2020"],
-      engineSize: "2.0",
-      cylinder: 4,
-      combustion: "Gasolina",
-      engineType: "Turbo",
-      files: [],
+      installationEndDate: "",
+      signImage: "base64EncodedImageString",
+      installationStatus: "pending",
+      tankStatus: 50,
+      mileage: 17800,
+      damageImages: [binaryImage, binaryImage],
+      scannerDescriptionImages: [binaryImage, binaryImage],
+      carId: "50be6810-1064-4cef-964b-ce2c99398829",
+      cognitoId: "1c124b64-e4ae-4a5a-8305-437e9be818bc",
+      damageStatusDescription: "Pequeños rayones",
+      scannerDescription: "No se encontraron errores",
+      errorCodes: [],
+      witnesses: [],
+      productInstalled: ["d0862f7f-fbef-4ff9-af86-c271014d906b"],
     }),
   } as any,
   {} as any,
--- test/Receipt/downloadReceipt.ts
+++ test/Receipt/downloadReceipt.ts
@@ -2,21 +2,22 @@ import { handler } from "../../src/infrastructure/web/routes/Receipt/handler";
 
 handler(
   {
-    pathParameters: {
-      receiptId: "75b8da5b-96be-4bac-bcb1-79418b06974a",
-    },
+    httpMethod: "GET",
     headers: {
       idtoken: process.env.ID_TOKEN,
       Authorization: process.env.AUTHORIZATION,
     },
     requestContext: {
       http: {
-        method: "PATCH",
+        method: "GET",
       },
     },
-    body: JSON.stringify({
-      carId: "50be6810-1064-4cef-964b-ce2c99398829",
-    }),
+    pathParameters: {
+      receiptId: "75b8da5b-96be-4bac-bcb1-79418b06974a", // Reemplaza con el ID del recibo que deseas descargar
+    },
+    queryStringParameters: {
+      download: true,
+    },
   } as any,
   {} as any,
 );
