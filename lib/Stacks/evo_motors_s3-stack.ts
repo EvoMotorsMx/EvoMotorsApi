15ede444a7370de8a29a996035f2dde866ef591b
Amilcar Sanchez <amilcaralex97@gmail.com>
2025-03-12 10:26:33 -0600

    Add S3 Access Point for sportDriveTemplates bucket to enhance access management

--- lib/Stacks/evo_motors_s3-stack.ts
+++ lib/Stacks/evo_motors_s3-stack.ts
@@ -1,11 +1,11 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
-
 interface EvoMotorsS3StackProps extends cdk.StackProps {}
 
 export class EvoMotorsS3Stack extends cdk.Stack {
   public readonly sportDriveTemplates: s3.Bucket;
+  public readonly sportDriveTemplatesAccessPoint: s3.CfnAccessPoint;
 
   constructor(scope: Construct, id: string, props?: EvoMotorsS3StackProps) {
     super(scope, id, props);
@@ -17,6 +17,27 @@ export class EvoMotorsS3Stack extends cdk.Stack {
       {
         versioned: true,
         removalPolicy: cdk.RemovalPolicy.RETAIN, // Retener el bucket cuando se elimine el stack
+      },
+    );
+
+    // Crear el Access Point para el bucket S3
+    this.sportDriveTemplatesAccessPoint = new s3.CfnAccessPoint(
+      this,
+      "SportDriveTemplatesAccessPoint",
+      {
+        bucket: this.sportDriveTemplates.bucketName,
+        name: "sport-drive-templates-access-point",
+        policy: {
+          version: "2012-10-17",
+          statement: [
+            {
+              effect: "Allow",
+              principal: "*",
+              action: "s3:GetObject",
+              resource: `arn:aws:s3:us-east-1:${this.account}:accesspoint/sport-drive-templates-access-point/object/*`,
+            },
+          ],
+        },
       },
     );
   }
