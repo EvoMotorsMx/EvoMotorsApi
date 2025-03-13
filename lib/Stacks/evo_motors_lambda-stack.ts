5dab145b3ab8d35ce52ba3676fdafa83293e4301
Amilcar Sanchez <amilcaralex97@gmail.com>
2025-03-13 10:22:17 -0600

    Remove unused properties and permissions related to PDF Lambda integration in LambdaStack

--- lib/Stacks/evo_motors_lambda-stack.ts
+++ lib/Stacks/evo_motors_lambda-stack.ts
@@ -5,13 +5,11 @@ import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 import { join } from "path";
 import * as s3 from "aws-cdk-lib/aws-s3";
-import * as iam from "aws-cdk-lib/aws-iam";
 
 interface LambdaStackProps extends StackProps {
   lambdaDirectory: string;
   envVariables: { [key: string]: string };
   bucket?: s3.Bucket;
-  readPdfLambdaName?: string;
 }
 
 export class LambdaStack extends Stack {
@@ -22,10 +20,6 @@ export class LambdaStack extends Stack {
 
     const environment = {
       ...props.envVariables,
-      ...(props.bucket && { BUCKET_NAME: props.bucket.bucketName }),
-      ...(props.readPdfLambdaName && {
-        READ_PDF_LAMBDA_NAME: props.readPdfLambdaName,
-      }),
     };
 
     const lambda = new NodejsFunction(this, "lambdaIntegration", {
@@ -48,23 +42,6 @@ export class LambdaStack extends Stack {
         nodeModules: ["@smithy/core"],
       },
     });
-
-    // Otorgar permisos de lectura al bucket S3 si se proporciona
-    if (props.bucket) {
-      props.bucket.grantRead(lambda);
-    }
-
-    // Otorgar permisos para invocar la función Lambda de lectura de PDF
-    if (props.readPdfLambdaName) {
-      lambda.addToRolePolicy(
-        new iam.PolicyStatement({
-          actions: ["lambda:InvokeFunction"],
-          resources: [
-            `arn:aws:lambda:${this.region}:${this.account}:function:${props.readPdfLambdaName}`,
-          ],
-        }),
-      );
-    }
 
     this.lambdaIntegration = new HttpLambdaIntegration(
       "httpLambdaIntegration",
