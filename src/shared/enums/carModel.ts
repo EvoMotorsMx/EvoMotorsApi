bf1d4c54ce35ea0109bd479d62dd54e9098d9146
Amilcar Sanchez <amilcaralex97@gmail.com>
2025-04-23 16:18:25 -0600

    REFACTOR(CAR): Enhance CarModel DTOs and schema with additional properties

--- src/shared/enums/carModel.ts
+++ src/shared/enums/carModel.ts
@@ -2,6 +2,8 @@ export enum EngineType {
   Aspirado = "Aspirado",
   Turbo = "Turbo",
   SuperCargado = "Super Cargado",
+  TurboCargado = "Turbo Cargado",
+  CumminsTdi = "CUMMINS TDI",
 }
 
 export enum CombustionType {
