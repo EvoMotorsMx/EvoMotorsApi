7f034600dc502f90a29c592f702fcfea2cbca0a9
Amilcar Sanchez <amilcaralex97@gmail.com>
2025-05-22 12:12:50 -0600

    feat: enhance CreateProductDTO and UpdateProductDTO with additional fields

--- src/core/application/dtos/Product/UpdateProduct.dto.ts
+++ src/core/application/dtos/Product/UpdateProduct.dto.ts
@@ -1,4 +1,13 @@
+import { ProductType, ProductSystemType } from "../../../../shared/enums";
+
 export type UpdateProductDTO = {
   name?: string;
+  type: ProductType;
   description?: string;
+  sku?: string;
+  productGroupId?: string;
+  productBrandId?: string;
+  systemType?: ProductSystemType;
+  stock?: number;
+  price?: number;
 };
