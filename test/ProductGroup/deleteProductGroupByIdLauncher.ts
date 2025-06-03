3797c4f66fcb778c8fc775fc6e3879ebfc2a1d36
Amilcar Sanchez <amilcaralex97@gmail.com>
2025-06-03 10:52:08 -0600

    feat: update pathParameters to use productGroupId in launcher files

--- test/ProductGroup/deleteProductGroupByIdLauncher.ts
+++ test/ProductGroup/deleteProductGroupByIdLauncher.ts
@@ -3,7 +3,7 @@ import { handler } from "../../src/infrastructure/web/routes/ProductGroup/handle
 handler(
   {
     pathParameters: {
-      productBrandId: "4f05e458-2476-4bb2-addc-fae54f22299e",
+      productGroupId: "4f05e458-2476-4bb2-addc-fae54f22299e",
     },
     headers: {
       idtoken: process.env.ID_TOKEN,
